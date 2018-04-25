e2b9e504377b4ad85ffed0075d668ea51a3af823
Cristian Reynoso <cris@Cris.local>
2018-04-25 13:27:59 -0400

    changes for testing

--- index.js
+++ index.js
@@ -3,27 +3,31 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
-require('./models/User');
-require('./services/passport');
+// require('./models/User');
+// require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000, // cookie will last 30 days in ms
-        keys: [keys.cookieKey],
-    })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(
+//     cookieSession({
+//         maxAge: 30 * 24 * 60 * 60 * 1000, // cookie will last 30 days in ms
+//         keys: [keys.cookieKey],
+//     })
+// );
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-require('./routes/authRoutes')(app);
+// require('./routes/authRoutes')(app);
 
 app.get('/', (req, res) => {
     res.send('Welcome!');
 });
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('is in production');
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
