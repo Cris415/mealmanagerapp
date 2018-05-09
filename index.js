1e53b3c3df8f90ae3ea22489b68288c694ce31de
Cristian <cristianreynoso@icloud.com>
2018-05-09 10:43:27 -0400

    Add server routes for production

--- index.js
+++ index.js
@@ -25,5 +25,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/recipeRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    //  Serve prodution assets like a particular file
+    app.use(express.static('client/build'));
+
+    // Serve index.html file for paths not in the server
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
