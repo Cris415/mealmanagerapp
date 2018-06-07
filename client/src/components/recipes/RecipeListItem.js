90718caba88c0b5c3ab1dcc4359b95c206afcaa7
Cristian <cristianreynoso@icloud.com>
2018-06-07 11:33:27 -0400

    Refactor some of the code in the render function into individual functions

--- client/src/components/recipes/RecipeListItem.js
+++ client/src/components/recipes/RecipeListItem.js
@@ -7,27 +7,60 @@ class RecipeListItem extends Component {
     state = { redirect: false };
     handleRecipeAdd = () => {
         this.props.addDateRecipe(this.props.date, this.props.recipe._id, () => {
+            // TODO: use the redirect component from react-router, change redirect to true.
             this.props.history.push('/dashboard');
         });
     };
-    render() {
-        const { recipe, date } = this.props;
+    renderImage = () => {
+        const { recipe } = this.props;
         const defaultImage = <i className="material-icons circle">image</i>;
+        return recipe.image ? (
+            <img src={recipe.image} alt={recipe.title} className="circle" />
+        ) : (
+            defaultImage
+        );
+    };
+    renderIngredients = () => {
+        // Only display the first three ingredients
+        return this.props.recipe.ingredients.map((ingredient, i, arr) => {
+            if (arr.length === i + 1 && arr.length < 4) {
+                return ingredient;
+            }
+            if (i + 1 > 2 && i < 3) {
+                return ingredient + '...';
+            }
+            if (i < 3) {
+                return ingredient + ', ';
+            }
+
+            return '';
+        });
+    };
+
+    renderRecipeAddButton = () => {
+        return this.props.date ? (
+            <button
+                onClick={this.handleRecipeAdd}
+                className="right btn "
+                style={{ display: 'inline-block' }}
+            >
+                <i className="material-icons ">add</i>
+            </button>
+        ) : (
+            <div />
+        );
+    };
+    render() {
+        const { recipe } = this.props;
+
         if (this.state.redirect) {
             return <Redirect to="/dashboard" />;
         }
         return (
             <div className="collection-item avatar">
                 <Link to={`/recipes/${recipe._id}`}>
-                    {recipe.image ? (
-                        <img
-                            src={recipe.image}
-                            alt={recipe.title}
-                            className="circle"
-                        />
-                    ) : (
-                        defaultImage
-                    )}
+                    {this.renderImage()}
+
                     <span className="black-text title">{recipe.title}</span>
                     <br />
                     <p
@@ -37,35 +70,19 @@ class RecipeListItem extends Component {
                             display: 'inline-block',
                         }}
                     >
-                        {recipe.ingredients.map((ingredient, i, arr) => {
-                            if (arr.length === i + 1 && arr.length < 4) {
-                                return ingredient;
-                            }
-                            if (i + 1 > 2 && i < 3) {
-                                return ingredient + '...';
-                            }
-                            if (i < 3) {
-                                return ingredient + ', ';
-                            }
-
-                            return '';
-                        })}
+                        {this.renderIngredients()}
                     </p>
                 </Link>
-                {date ? (
-                    <button
-                        onClick={this.handleRecipeAdd}
-                        className="right btn "
-                        style={{ display: 'inline-block' }}
-                    >
-                        <i className="material-icons ">add</i>
-                    </button>
-                ) : (
-                    <div />
-                )}
+
+                {this.renderRecipeAddButton()}
             </div>
         );
     }
 }
 
-export default withRouter(connect(null, { addDateRecipe })(RecipeListItem));
+export default withRouter(
+    connect(
+        null,
+        { addDateRecipe }
+    )(RecipeListItem)
+);
